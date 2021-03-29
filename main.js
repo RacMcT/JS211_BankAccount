99afefd54dfe99c21e7f919f32c973050c5a56ba
RacMcT <raccmct@gmail.com>
2021-03-28 20:03:43 -0500

    finished bank account functions

--- main.js
+++ main.js
@@ -5,28 +5,78 @@ class BankAccount{
     this.transactions = [];
   }
 
-balance(amt) // not sure, but links to Transactions array and then does sum()?
-
-deposit(amt) => {
+balance(){
+  let result = this.transactions.reduce((acc, transact) => acc + transact.amount , 0)
+  console.log(result.toFixed(2))
+}
+deposit(amt){
   this.amt = parseInt(amt);
-  if (this.transactions.amt <= 0){
-    return ("Need to deposit positive balance")
-  } else {
-    this.transactions.amt.push(this);
-    return ("thank you for your deposit of: " + this.amt)
+  if (amt <= 0){
+    console.log ("Please note, you need to deposit positive balance")
+  } 
+  if (amt > 0){
+    let transaction = new Transaction(amt, this.owner)
+    console.log ("thank you for your deposit of: " + this.amt)
   }
-
-
+  charge(payee, amt);{
+    let amount = Math.abs(amt) * -1
+    let transaction = new Transaction(amount, payee)
+    this.transactions.push(transaction)
+  }
+}
 }
 
-charge(payee, amt){
-    this.payee = payee;
+class Transaction{
+  constructor (amt, payee){
     this.amt = amt;
-    this.payee.push(this);
-}
+    this.payee = payee;
+    this.date = new Date();
+  }
+  }
 
-  }
-  }
+let rachel = new BankAccount(1111, 'Rachel McTavish');
+let qwill = new BankAccount(1112, 'Qwilleran McTavish');
+
+qwill.deposit(10000)
+qwill.balance()
+rachel.deposit(4.75)
+rachel.deposit(174.52)
+rachel.deposit(-8.50)
+rachel.charge('Jean-Luc Picard', 14.99)
+rachel.balance()
+
+
+//FIRST CODE ATTEMPTS/NOTES:
+// charge(payee, amt){
+//     this.payee = payee;
+//     this.amt = amt;
+//     this.payee.push(this);
+// }
+
+//   }
+
+  // balance(amt) // not sure, but links to Transactions array and then does sum()?
+
+//     balance(){
+//       let result = this.transactions.reduce((acc, transact) => acc + transact.amount , 0)
+//       console.log(result.toFixed(2))
+//     }
+//     deposit(amt){
+//       if (amt > 0){
+//         let transaction = new Transaction(amt, this.owner)
+//         this.transactions.push(transaction)
+//       }
+//       if (amt <= 0){
+//         console.log(`If you wish to deposit you must input a positive amount.`) 
+//       }
+//     }
+//     charge(payee, amt){
+//       let amount = Math.abs(amt) * -1
+//       let transaction = new Transaction(amount, payee)
+//       this.transactions.push(transaction)
+//     }
+//   }
+  
 
 
 //Functions for BankAccount Class:
@@ -45,11 +95,6 @@ charge(payee, amt){
 // NOTE: You should not be able to charge an amount that would make your balance dip below 0
 
 
-class Transactions extends BankAccount{
-  constructor (date, amt, payee){
-    this.amt = amt;
-    this.payee = payee;
-    this.date = new Date();
-  }
-  }
-}
+
+
+
